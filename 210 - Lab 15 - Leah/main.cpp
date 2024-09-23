4da8d37862930c1409b12d455934f014db2282cb
leahbgithub <leahchaku@gmail.com>
2024-09-23 14:44:57 -0700

    Commit once!

--- 210 - Lab 15 - Leah/main.cpp
+++ 210 - Lab 15 - Leah/main.cpp
@@ -1,1 +1,31 @@
 // 210 - Lab 15 - Leah Balakrishnan
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Movie {
+private:
+    string title;
+    int yearReleased;
+    string screenWriter;
+    
+public:
+    // Below are the setters
+    void setTitle(const string& title) {
+        this->title = title;
+    }
+    
+    void setYearReleased(int yearReleased) {
+        this->yearReleased = yearReleased;
+    }
+    
+    void setScreenWriter(const string& screenWriter) {
+        this->screenWriter = screenWriter;
+    }
+    
+    // Below are the getters
+    void
+}
